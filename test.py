1a240c84c2a8ae3d7673225515f25c650a802406
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-08-09 22:57:09 +0900

    [update]fizzbuzz game

--- test.py
+++ test.py
@@ -3,15 +3,29 @@ from re import I
 
 i = int(input("数字を入力してください"))
 n = i != 0
-if i % 15 == 0 and n:
-  print("fizzbuzz")
-elif i % 3 == 0 and n:
-  print("fizz")
-elif i % 5 == 0 and n:
-  print("buzz")
-else:
-  if i == 0:
-    print("0です")
+fiz=0
+buz=0
+fizbuz=0
+for num in range(i):
+  print(num)
+  if num % 15 == 0 and n:
+    print("fizzbuzz")
+    fizbuz +=1
+  elif num % 3 == 0 and n:
+    print("fizz")
+    fiz+=1
+  elif num % 5 == 0 and n:
+    print("buzz")
+    buz+=1
   else:
-    print("どちらにもあてはまりません")
-
+    if i == 0:
+      print("0です")
+    else:
+      print("どちらにもあてはまりません")
+fb=str(fizbuz)
+fz=str(fiz)
+bz=str(buz)
+print("結果は")
+print("fizzbuzz" + fb)
+print("fizz" + fz)
+print("buzz" + bz)
