a0aac7ab17deb036e97bfbe5a950eb56826a2f41
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-08-09 16:11:40 +0900

    [update]奇数、偶数判別

--- for_study.py
+++ for_study.py
@@ -1,5 +1,9 @@
 import random as rnd
 i=int(input("整数を入力"))
 for num in range(i):
-  print(num)
+  number = str(num)
+  if num % 2 == 0:
+    print(number + "偶数")
+  else:
+    print(number + "奇数")
 print(rnd.randrange(num))
