2985f791bf100b3f0d25efa6dcbee4cc3b70556c
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-08-10 18:39:41 +0900

    [update]join学習

--- function_study.py
+++ function_study.py
@@ -1,3 +1,4 @@
+
 a=int(input("a"))
 b=int(input("b"))
 c=range(a,b)
@@ -5,3 +6,8 @@ print(len(c))
 print(min(c))
 print(max(c))
 print(sum(c))
+f_name=input("姓")
+l_name=input("名")
+age=input("年齢")
+fullname=[f_name,l_name]
+print("".join(fullname)+"さんは"+age+"歳ですね？")
