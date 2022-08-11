8c13e236849c663b42ba03485b5522e58541f799
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-08-11 16:21:00 +0900

    [update]

--- function_study.py
+++ function_study.py
@@ -1,4 +1,7 @@
 
+from pathlib import PureWindowsPath
+
+
 a=int(input("a"))
 b=int(input("b"))
 c=range(a,b)
@@ -6,8 +9,16 @@ print(len(c))
 print(min(c))
 print(max(c))
 print(sum(c))
-f_name=input("姓")
-l_name=input("名")
-age=input("年齢")
-fullname=[f_name,l_name]
-print("".join(fullname)+"さんは"+age+"歳ですね？")
+
+def human():
+  f_name=input("姓")
+  l_name=input("名")
+  age=input("年齢")
+  fullname=[f_name,l_name]
+  print("".join(fullname)+"さんは"+age+"歳ですね？")
+human()
+d=input("正しいですか？y/n?")
+if d == "n":
+  human()
+else:
+  print("終了")
