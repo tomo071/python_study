1cddb4811f8536a3afdabee6c2a57107ad761c7d
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-08-13 03:48:46 +0900

    update

--- bmi.py
+++ bmi.py
@@ -2,6 +2,7 @@ w=int(input("あなたの体重は？"))
 h=int(input("あなたの身長は？"))*0.01
 height=h**2
 bmi=w/h
+bestweight=str(height*22-w)
 best=str(height*22)
 print("あなたのBMIは"+str(bmi)+"です")
 if bmi<18.5:
@@ -11,3 +12,4 @@ elif 18.5<=bmi and bmi<25.0:
 else:
   print("肥満傾向にあります")
 print("あなたの適正体重は"+best+"です")
+print("適正体重まで"+bestweight+"kg")
