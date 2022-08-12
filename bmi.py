8fa858e80e50018f71ac39764b6d0c81a97b84f8
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-08-12 17:07:23 +0900

    [add]bmi計算作成

---
+++ bmi.py
@@ -0,0 +1,13 @@
+w=int(input("あなたの体重は？"))
+h=int(input("あなたの身長は？"))*0.01
+height=h**2
+bmi=w/h
+best=str(height*22)
+print("あなたのBMIは"+str(bmi)+"です")
+if bmi<18.5:
+  print("低体重の傾向があります")
+elif 18.5<=bmi and bmi<25.0:
+  print("適正体重です")
+else:
+  print("肥満傾向にあります")
+print("あなたの適正体重は"+best+"です")
