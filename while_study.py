b2d68b507e6c4e41032451cdb89c241775b8dce0
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-08-17 19:55:46 +0900

    update

--- while_study.py
+++ while_study.py
@@ -1,9 +1,13 @@
 import random as rnd
-num=int(input("整数を入力"))
-count=0
-random=-1
-max = 1000
-while random != num:
-  random = rnd.randrange(max)
-  count += 1
-print("%d回目に%dが出ました"% (count,num))
+num = int(input("整数を入力"))
+count = 0
+random = -1
+max = int(input("max"))
+if num<max:
+  while random != num:
+    random = rnd.randrange(max)
+    print(random)
+    count += 1
+  print("%d回目に%dが出ました"% (count,num))
+else:
+  print("無限ループが発生します")
