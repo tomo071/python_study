58f2c92e07b09d4c5ee3405e474cd70e1c09a35c
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-08-20 17:05:47 +0900

    update

--- for_study.py
+++ for_study.py
@@ -1,14 +1,22 @@
+from asyncio import events
 import random as rnd
 i=int(input("整数を入力"))
+even=0
+odd=0
 for num in range(i):
   number = str(num)
   if num % 2 == 0:
+    even+=1
     print(number + "偶数")
   else:
+    odd+=1
     print(number + "奇数")
 print(rnd.randrange(num))
+print("奇数%d回"%odd)
+print("偶数%d回"%even)
 w=0
 while w<10:
   w+=1
   print(w,end="")
 print(w)
+
