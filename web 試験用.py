bc5eac1093439e06133fc94c6867d6fc02bf9346
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-10-03 22:03:18 +0900

    [add]

--- web 試験用.py
+++ web 試験用.py
@@ -1,4 +1,13 @@
-
-a = int(input("a"))
-b = int(input("b"))
+x1 = float(1)
+x2 = float(2)
+x3 = float(3)
+a = float(0)
+i = 0
+while i == 10000:
+  a = a + x1 * x2 / x3
+  print("答えは%d"%a)
+  x1 += 3
+  x2 += 3
+  x3 += 3
+  i += 1
 
